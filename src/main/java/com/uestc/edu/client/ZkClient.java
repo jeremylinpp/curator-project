633542c1494a89254f72bf55d593e9752c7bb913
linchen8 <linchen8@lenovo.com>
2023-07-19 16:40:49 +0800

    init

--- src/main/java/com/uestc/edu/client/ZkClient.java
+++ src/main/java/com/uestc/edu/client/ZkClient.java
@@ -3,6 +3,9 @@ package com.uestc.edu.client;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+
+import java.util.List;
 
 /**
  * @ClassName: ZkClient.java
@@ -11,7 +14,6 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
  * @Date: 2022/9/30 11:17
  **/
 public class ZkClient {
-
     public static CuratorFramework getClient() {
         return CuratorFrameworkFactory.builder()
                 .connectString("127.0.0.1:2181")
@@ -25,9 +27,36 @@ public class ZkClient {
                 .build();
     }
 
-    public static void main(String[] args) {
-        CuratorFramework client = getClient();
-        System.out.println(client.toString());
+    public static void create(final CuratorFramework client, final String path, final byte[] payload) throws Exception {
+        client.create().creatingParentsIfNeeded().forPath(path, payload);
+    }
+
+    public static void createEphemeral(final CuratorFramework client, final String path, final byte[] payload) throws Exception {
+        client.create().withMode(CreateMode.EPHEMERAL).forPath(path, payload);
+    }
+
+    public static String createEphemeralSequential(final CuratorFramework client, final String path, final byte[] payload) throws Exception {
+        return client.create().withProtection().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath(path, payload);
+    }
+
+    public static void setData(final CuratorFramework client, final String path, final byte[] payload) throws Exception {
+        client.setData().forPath(path, payload);
+    }
+
+    public static void delete(final CuratorFramework client, final String path) throws Exception {
+        client.delete().deletingChildrenIfNeeded().forPath(path);
+    }
+
+    public static void guaranteedDelete(final CuratorFramework client, final String path) throws Exception {
+        client.delete().guaranteed().forPath(path);
+    }
+
+    public static String getData(final CuratorFramework client, final String path) throws Exception {
+        return new String(client.getData().forPath(path));
+    }
+
+    public static List<String> getChildren(final CuratorFramework client, final String path) throws Exception {
+        return client.getChildren().forPath(path);
     }
 
 }
